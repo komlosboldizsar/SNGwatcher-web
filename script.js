12acb5073a92fbe24d9ca746cd445995be0afdd5
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2025-06-15 19:35:48 +0200

    Show if no data from server/IRD

--- script.js
+++ script.js
@@ -1,29 +1,54 @@
-function updateSngData() {
-	$.get('https://sngwatcher.kboldi.hu/get.php?data=current&sng=' + SNG_ID, function(dataString) {
+var updateCurrentDataErrorCounter = 0;
+var updateCurrentDataTooOld = false;
+
+function updateCurrentDataSuccess(dataString) {
+	
+	var data = JSON.parse(dataString);
 		
-		var data = JSON.parse(dataString);
-		
-		var valueCN = data.cn/100;
-		document.getElementById('field-cn').innerText = valueCN.toFixed(1) + ' dB';
-		var blockCN = document.getElementById('block-cn');
-		blockCN.classList.remove('good');
-		blockCN.classList.remove('warning');
-		blockCN.classList.remove('bad');
-		if ((valueCN >= SETTINGS.ranges.good.min) && (valueCN <= SETTINGS.ranges.good.max))
-			blockCN.classList.add('good');
-		else if ((valueCN >= SETTINGS.ranges.warning.min) && (valueCN <= SETTINGS.ranges.warning.max))
-			blockCN.classList.add('warning');
-		else
-			blockCN.classList.add('bad');
-		
-		var valueTimestamp = Date.parse(data.Timestamp);
-		document.getElementById('field-timestamp').innerText = (new Date(valueTimestamp)).toLocaleString();
-		if ((Date.now() - valueTimestamp) > 10 * 1000)
-			document.getElementById('row-timestamp').classList.add('warning');
-		else
-			document.getElementById('row-timestamp').classList.remove('warning');
-		
-	});
+	var valueCN = data.cn/100;
+	document.getElementById('field-cn').innerText = valueCN.toFixed(1) + ' dB';
+	var blockCN = document.getElementById('block-cn');
+	blockCN.classList.remove('good');
+	blockCN.classList.remove('warning');
+	blockCN.classList.remove('bad');
+	if ((valueCN >= SETTINGS.ranges.good.min) && (valueCN <= SETTINGS.ranges.good.max))
+		blockCN.classList.add('good');
+	else if ((valueCN >= SETTINGS.ranges.warning.min) && (valueCN <= SETTINGS.ranges.warning.max))
+		blockCN.classList.add('warning');
+	else
+		blockCN.classList.add('bad');
+	
+	var valueTimestamp = Date.parse(data.Timestamp);
+	document.getElementById('field-timestamp').innerText = (new Date(valueTimestamp)).toLocaleString();
+	updateCurrentDataTooOld = ((Date.now() - valueTimestamp) > 10 * 1000);
+	if (updateCurrentDataTooOld)
+		document.getElementById('row-timestamp').classList.add('warning');
+	else
+		document.getElementById('row-timestamp').classList.remove('warning');
+	
+	updateCurrentDataErrorCounter = 0;
+	updateCurrentDataErrorHandler();
+	
+}
+
+function updateCurrentDataError() {
+	updateCurrentDataErrorCounter++;
+	updateCurrentDataErrorHandler();
+}
+
+function updateCurrentDataErrorHandler() {
+	if (updateCurrentDataErrorCounter > 3) {
+		document.getElementById('block-error-server').style.display = 'block';
+		document.getElementById('block-error-ird').style.display = 'none';
+	} else {
+		document.getElementById('block-error-server').style.display = 'none';
+		document.getElementById('block-error-ird').style.display = updateCurrentDataTooOld ? 'block' : 'none';
+	}
+}
+
+function updateCurrentData() {
+	$.get('https://sngwatcher.kboldi.hu/get.php?data=current&sng=' + SNG_ID)
+     .done(updateCurrentDataSuccess);
 }
 
 // This part not working, using fixed settings
@@ -47,8 +72,8 @@ var SETTINGS = {
 };
 
 $(function(){
-	updateSngData();
-	setInterval(updateSngData, 1000);
+	updateCurrentData();
+	setInterval(updateCurrentData, 1000);
 });
 
 
