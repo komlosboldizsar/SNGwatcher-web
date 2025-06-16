beb5a2f4814000514ed212df6cb39474879c8a73
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2025-06-16 13:30:16 +0200

    Fix: rename index.html to .php

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php $SNG_ID = $_GET['sng']; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+	<title>SNGwatcher</title>
+	<meta name="viewport" content="width=device-width,initial-scale=1" />
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="style.css" />
+	<link rel="icon" href="favicon.ico">
+</head>
+<body>
+	<div id="middle">
+		<div id="block-cn" class="block">
+			<div id="field-cn">??</div>
+		</div>
+		<div id="block-data" class="block">
+			<table>
+				<tr>
+					<td>SNG:</td>
+					<td id='field-sng'><?php echo $SNG_ID; ?></td>
+					<td>&nbsp;</td>
+				</tr>
+				<tr id="row-timestamp">
+					<td>Timestamp:</td>
+					<td id='field-timestamp'>-</td>
+				</tr>
+			</table>
+		</div>
+		<div id="block-error-server" class="block block-error">
+			No Data from Server
+		</div>
+		<div id="block-error-ird" class="block block-error">
+			No Data from IRD
+		</div>
+	</div>
+	<script type="text/javascript">
+		var SNG_ID = '<?php echo $SNG_ID; ?>';
+	</script>
+	<script src="https://code.jquery.com/jquery-3.7.1.min.js" crossorigin="anonymous"></script>
+	<script src="script.js"></script>
+</body>
+</html>
